342e8785dcfa0f0f2f931521eb2482a16ad23941
epradeep <etikapradeep@gmail.com>
2026-08-16 00:25:44 +0530

    feat: initialize app with React TypeScript and application shell

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>PeopleHub HRMS</h1>
+      <p>Human Resource Management System</p>
+    </>
+  );
+}
+
+export default App;
